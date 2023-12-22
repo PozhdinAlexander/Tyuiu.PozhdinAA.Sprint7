de5e016ae51dea9dee84ca112a74b8e4989b26ff
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-12-22 22:40:47 +0500

    rework3

--- Tyuiu.PozhdinAA.Sprint7.Project.V12/FormStatistic.Designer.cs
+++ Tyuiu.PozhdinAA.Sprint7.Project.V12/FormStatistic.Designer.cs
@@ -29,12 +29,218 @@ namespace Tyuiu.PozhdinAA.Sprint7.Project.V12
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.panelStatistic_KKA = new System.Windows.Forms.Panel();
+            this.textBoxAvgPrice_PAA = new System.Windows.Forms.TextBox();
+            this.labelAvgPrice_PAA = new System.Windows.Forms.Label();
+            this.textBoxMaxPrice_PAA = new System.Windows.Forms.TextBox();
+            this.labelMaxPrice_PAA = new System.Windows.Forms.Label();
+            this.textBoxMinPrice_PAA = new System.Windows.Forms.TextBox();
+            this.labelMin_PAA = new System.Windows.Forms.Label();
+            this.tabControlGraph_PAA = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelStatistic_KKA.SuspendLayout();
+            this.tabControlGraph_PAA.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelStatistic_KKA
+            // 
+            this.panelStatistic_KKA.BackColor = System.Drawing.Color.DarkOrange;
+            this.panelStatistic_KKA.Controls.Add(this.textBoxAvgPrice_PAA);
+            this.panelStatistic_KKA.Controls.Add(this.labelAvgPrice_PAA);
+            this.panelStatistic_KKA.Controls.Add(this.textBoxMaxPrice_PAA);
+            this.panelStatistic_KKA.Controls.Add(this.labelMaxPrice_PAA);
+            this.panelStatistic_KKA.Controls.Add(this.textBoxMinPrice_PAA);
+            this.panelStatistic_KKA.Controls.Add(this.labelMin_PAA);
+            this.panelStatistic_KKA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelStatistic_KKA.Location = new System.Drawing.Point(0, 0);
+            this.panelStatistic_KKA.Name = "panelStatistic_KKA";
+            this.panelStatistic_KKA.Size = new System.Drawing.Size(235, 450);
+            this.panelStatistic_KKA.TabIndex = 1;
+            // 
+            // textBoxAvgPrice_PAA
+            // 
+            this.textBoxAvgPrice_PAA.BackColor = System.Drawing.Color.White;
+            this.textBoxAvgPrice_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAvgPrice_PAA.ForeColor = System.Drawing.Color.Black;
+            this.textBoxAvgPrice_PAA.Location = new System.Drawing.Point(18, 190);
+            this.textBoxAvgPrice_PAA.Name = "textBoxAvgPrice_PAA";
+            this.textBoxAvgPrice_PAA.ReadOnly = true;
+            this.textBoxAvgPrice_PAA.Size = new System.Drawing.Size(200, 35);
+            this.textBoxAvgPrice_PAA.TabIndex = 5;
+            this.textBoxAvgPrice_PAA.TabStop = false;
+            // 
+            // labelAvgPrice_PAA
+            // 
+            this.labelAvgPrice_PAA.AutoSize = true;
+            this.labelAvgPrice_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAvgPrice_PAA.ForeColor = System.Drawing.Color.Black;
+            this.labelAvgPrice_PAA.Location = new System.Drawing.Point(13, 156);
+            this.labelAvgPrice_PAA.Name = "labelAvgPrice_PAA";
+            this.labelAvgPrice_PAA.Size = new System.Drawing.Size(205, 30);
+            this.labelAvgPrice_PAA.TabIndex = 4;
+            this.labelAvgPrice_PAA.Text = "Средняя стоимость:";
+            // 
+            // textBoxMaxPrice_PAA
+            // 
+            this.textBoxMaxPrice_PAA.BackColor = System.Drawing.Color.White;
+            this.textBoxMaxPrice_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMaxPrice_PAA.ForeColor = System.Drawing.Color.Black;
+            this.textBoxMaxPrice_PAA.Location = new System.Drawing.Point(18, 118);
+            this.textBoxMaxPrice_PAA.Name = "textBoxMaxPrice_PAA";
+            this.textBoxMaxPrice_PAA.ReadOnly = true;
+            this.textBoxMaxPrice_PAA.Size = new System.Drawing.Size(200, 35);
+            this.textBoxMaxPrice_PAA.TabIndex = 3;
+            this.textBoxMaxPrice_PAA.TabStop = false;
+            // 
+            // labelMaxPrice_PAA
+            // 
+            this.labelMaxPrice_PAA.AutoSize = true;
+            this.labelMaxPrice_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMaxPrice_PAA.ForeColor = System.Drawing.Color.Black;
+            this.labelMaxPrice_PAA.Location = new System.Drawing.Point(13, 85);
+            this.labelMaxPrice_PAA.Name = "labelMaxPrice_PAA";
+            this.labelMaxPrice_PAA.Size = new System.Drawing.Size(179, 30);
+            this.labelMaxPrice_PAA.TabIndex = 2;
+            this.labelMaxPrice_PAA.Text = "Макс. стоимость:";
+            // 
+            // textBoxMinPrice_PAA
+            // 
+            this.textBoxMinPrice_PAA.BackColor = System.Drawing.Color.White;
+            this.textBoxMinPrice_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMinPrice_PAA.ForeColor = System.Drawing.Color.Black;
+            this.textBoxMinPrice_PAA.Location = new System.Drawing.Point(18, 47);
+            this.textBoxMinPrice_PAA.Name = "textBoxMinPrice_PAA";
+            this.textBoxMinPrice_PAA.ReadOnly = true;
+            this.textBoxMinPrice_PAA.Size = new System.Drawing.Size(200, 35);
+            this.textBoxMinPrice_PAA.TabIndex = 1;
+            this.textBoxMinPrice_PAA.TabStop = false;
+            // 
+            // labelMin_PAA
+            // 
+            this.labelMin_PAA.AutoSize = true;
+            this.labelMin_PAA.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMin_PAA.ForeColor = System.Drawing.Color.Black;
+            this.labelMin_PAA.Location = new System.Drawing.Point(13, 13);
+            this.labelMin_PAA.Name = "labelMin_PAA";
+            this.labelMin_PAA.Size = new System.Drawing.Size(172, 30);
+            this.labelMin_PAA.TabIndex = 0;
+            this.labelMin_PAA.Text = "Мин. стоимость:";
+            // 
+            // tabControlGraph_PAA
+            // 
+            this.tabControlGraph_PAA.Controls.Add(this.tabPage1);
+            this.tabControlGraph_PAA.Controls.Add(this.tabPage2);
+            this.tabControlGraph_PAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlGraph_PAA.Location = new System.Drawing.Point(235, 0);
+            this.tabControlGraph_PAA.Name = "tabControlGraph_PAA";
+            this.tabControlGraph_PAA.SelectedIndex = 0;
+            this.tabControlGraph_PAA.Size = new System.Drawing.Size(565, 450);
+            this.tabControlGraph_PAA.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(557, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Круговая";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(557, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Столбчатая";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(-4, 0);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(561, 424);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(3, 3);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(551, 418);
+            this.chart2.TabIndex = 1;
+            title2.Name = "График";
+            this.chart2.Titles.Add(title2);
+            // 
+            // FormStatistic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tabControlGraph_PAA);
+            this.Controls.Add(this.panelStatistic_KKA);
+            this.Name = "FormStatistic";
+            this.Text = "Статистика ЭВМ";
+            this.panelStatistic_KKA.ResumeLayout(false);
+            this.panelStatistic_KKA.PerformLayout();
+            this.tabControlGraph_PAA.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelStatistic_KKA;
+        private System.Windows.Forms.TextBox textBoxAvgPrice_PAA;
+        private System.Windows.Forms.Label labelAvgPrice_PAA;
+        private System.Windows.Forms.TextBox textBoxMaxPrice_PAA;
+        private System.Windows.Forms.Label labelMaxPrice_PAA;
+        private System.Windows.Forms.TextBox textBoxMinPrice_PAA;
+        private System.Windows.Forms.Label labelMin_PAA;
+        private System.Windows.Forms.TabControl tabControlGraph_PAA;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
