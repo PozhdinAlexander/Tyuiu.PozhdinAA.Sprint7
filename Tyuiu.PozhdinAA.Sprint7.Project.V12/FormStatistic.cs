d23bd3f4c11e85b1d5e00212d9516502934f4f41
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-12-23 00:33:42 +0500

    Final Rework

--- Tyuiu.PozhdinAA.Sprint7.Project.V12/FormStatistic.cs
+++ Tyuiu.PozhdinAA.Sprint7.Project.V12/FormStatistic.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tyuiu.PozhdinAA.Sprint7.Project.V12.Lib;
 
 namespace Tyuiu.PozhdinAA.Sprint7.Project.V12
 {
@@ -15,6 +16,32 @@ namespace Tyuiu.PozhdinAA.Sprint7.Project.V12
         public FormStatistic()
         {
             InitializeComponent();
+            var ds = new DataService();
+            var priceColumnIndex = 7;
+            var pathPC = @"..\Back-end\personal_computer.csv";
+            var data = ds.GetData(pathPC);
+            var prices = new double[data.GetLength(0)];
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                var priceString = data[i, priceColumnIndex].Replace('.', ',');
+                var parseSuccess = double.TryParse(priceString, out double price);
+                if (!parseSuccess)
+                {
+                    MessageBox.Show("Цена имеет неверный формат");
+                    return;
+                }
+
+                prices[i] = price;
+            }
+
+            this.textBoxMinPrice_PAA.Text = prices.Min().ToString();
+            this.textBoxMaxPrice_PAA.Text = prices.Max().ToString();
+            this.textBoxAvgPrice_PAA.Text = prices.Average().ToString();
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                this.chart1.Series[0].Points.AddXY(data[i, 0], data[i, 7]);
+                this.chart2.Series[0].Points.AddXY(data[i, 0], data[i, 7]);
+            }
         }
     }
 }
